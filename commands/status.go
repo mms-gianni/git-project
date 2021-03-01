b2f3274c641f0ffa103f9dd9a62767a829d26e46
mms-gianni <gianni.carafa@srf.ch>
2021-03-01 22:15:31 +0100

    rename ls to status

--- commands/status.go
+++ commands/status.go
@@ -11,8 +11,7 @@ func cmdList() *clif.Command {
 		githubcommands.GetItems(c)
 	}
 
-	return clif.NewCommand("ls", "List all Todo", cb).
-		NewArgument("status", "filter by status", "", false, false).
+	return clif.NewCommand("status", "List all Todo", cb).
 		NewArgument("repo", "Show only repo", "", false, false)
 }
 
